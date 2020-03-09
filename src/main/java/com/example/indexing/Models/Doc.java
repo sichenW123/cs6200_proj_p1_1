8d89ad61b34e0fa5843826799c5889b96880ab32
sichenwang <willwang217@gmail.com>
2020-03-08 22:09:49 -0700

    commit ranking

---
+++ src/main/java/com/example/indexing/Models/Doc.java
@@ -0,0 +1,7 @@
+package com.example.indexing.Models;
+
+/**
+ * Created by Enzo Cotter on 3/6/20.
+ */
+public class Doc {
+}
---
+++ src/main/java/com/example/indexing/Models/Index.java
@@ -0,0 +1,7 @@
+package com.example.indexing.Models;
+
+/**
+ * Created by Enzo Cotter on 3/1/20.
+ */
+public class Index {
+}
